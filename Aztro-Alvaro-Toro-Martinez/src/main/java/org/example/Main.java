fd00ce2f4dd0ecfefadc352c5b7152b7ce9d568a
toro-martinez-alvaro <torom.alvaro@gmail.com>
2024-10-09 00:06:27 -0500

    adding java project

---
+++ Aztro-Alvaro-Toro-Martinez/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.domains.Prueba;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Prueba("Esta es la prueba").getTextoDePrueba());
+        System.out.println("Hello world!");
+    }
+}
---
+++ Aztro-Alvaro-Toro-Martinez/src/main/java/org/example/domains/Prueba.java
@@ -0,0 +1,12 @@
+package org.example.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Prueba {
+    private String textoDePrueba;
+}
